c8c926398ae0130d65990761b0dbf8722d00571b
Thi-Trann <91626042+Thi-Trann@users.noreply.github.com>
2022-05-31 00:54:14 +0700

    Fixed bugs

---
+++ build/web/js/main.js
@@ -0,0 +1,54 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+const popup = document.querySelector('.chat-popup');
+const chatBtn = document.querySelector('.chat-btn');
+const closeBtn = document.querySelector('.close-btn');
+const submitBtn = document.querySelector('.submit-btn');
+const chatArea = document.querySelector('.chat-area');
+const txtMsg = document.querySelector('#txtmsg');
+
+//Display chat box
+$(popup).hide();
+
+$(chatBtn).click(function () {
+    $(popup).toggle("swing");
+    $(txtMsg).select();
+    eraseText();
+});
+
+$(closeBtn).click(function () {
+    $(popup).hide("swing");
+    eraseText();
+});
+
+//Send messages
+function eraseText() {
+    txtMsg.value = "";
+}
+
+$(txtMsg).keyup(function(event) {
+    if(event.keyCode === 13) {
+        $(submitBtn).click();
+    }
+});
+
+$(submitBtn).click(function () {
+    var userInput = $(txtMsg).val();
+    if (userInput === "") {
+        return;
+    } else {
+        var myMsg = `
+        <div class="out-msg">
+        <span class="my-msg">${userInput}</span>
+        </div>`;
+
+        chatArea.insertAdjacentHTML("beforeend", myMsg);
+        this.scrollIntoView(false);
+        userInput = "";
+        eraseText();
+        chatArea.scrollTop = chatArea.scrollHeight;
+    }
+});
